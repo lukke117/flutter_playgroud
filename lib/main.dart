21e247c03e8371c798af390faf896ffc669fbc57
lukke117 <@gumbechelsea@gmail.com>
2021-03-29 12:42:39 +0800

    color

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,71 @@
-import 'package:flutter/foundation.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class Todo {
-  final String title;
-  final String description;
-
-  Todo(this.title, this.description);
-}
-
 void main() {
-  runApp(MaterialApp(
-    title: 'Passing Data',
-    home: TodosScreen(
-      todos: List.generate(
-        20,
-            (i) => Todo(
-          'Todo $i',
-          'A description of what needs to be done for Todo $i',
-        ),
-      ),
-    ),
-  ));
+  runApp(MyApp());
 }
 
-class TodosScreen extends StatelessWidget {
-  final List<Todo> todos;
-
-  TodosScreen({Key key, @required this.todos}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Todos'),
+    return MaterialApp(
+      home: Scaffold(
+        body: BodyWidget(),
       ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todos[index].title),
-            // When a user taps the ListTile, navigate to the DetailScreen.
-            // Notice that you're not only creating a DetailScreen, you're
-            // also passing the current todo through to it.
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailScreen(),
-                  // Pass the arguments as part of the RouteSettings. The
-                  // DetailScreen reads the arguments from these settings.
-                  settings: RouteSettings(
-                    arguments: todos[index],
-                  ),
+    );
+  }
+}
+
+// TODO: When you press the button, change the background color of the app
+class BodyWidget extends StatelessWidget {
+  //String fruit = 'Apples';
+  final colorNotfier = ValueNotifier<Color>(Colors.purple.shade700);
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<Color>(
+      valueListenable: colorNotfier,
+      builder: (context, color, child) {
+        return Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: color,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Text('Hello'),
+                SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () {
+                    print('I was clicked.');
+                    final randomNumber = Random().nextInt(7);
+                    final myNewColor = colorList[randomNumber];
+
+                    colorNotfier.value = myNewColor;
+                  },
+                  child: Text('Click me'),
                 ),
-              );
-            },
-          );
-        },
-      ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final Todo todo = ModalRoute.of(context).settings.arguments;
 
-    // Use the Todo to create the UI.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(todo.title),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(todo.description),
-      ),
-    );
-  }
-}
+final colorList = <Color>[
+  Colors.purple,
+  Colors.white,
+  Colors.orange,
+  Colors.blue,
+  Colors.red,
+  Colors.pink,
+  Colors.lightGreenAccent,
+];
